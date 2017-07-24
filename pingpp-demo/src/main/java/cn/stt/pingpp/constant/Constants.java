b90a6417f0b9065cb124b226294ada9012b5e42c
shitongtong <shitongtong@onlyedu.com>
2017-07-24 12:00:57 +0800

    ping++ demo

---
+++ pingpp-demo/src/main/java/cn/stt/pingpp/constant/Constants.java
@@ -0,0 +1,48 @@
+package cn.stt.pingpp.constant;
+
+/**
+ * @Author shitongtong
+ * <p>
+ * Created by shitongtong on 2017/7/21.
+ */
+public interface Constants {
+
+    /**
+     * Secret key
+     */
+    String apiKey = "sk_test_54mXT88G84eD8S8WX9LS8WjD";
+
+    /**
+     * 应用标识
+     */
+    String appId = "app_qPmjfP5mT0y5rDy5";
+
+    /**
+     * RSA 私钥
+     */
+    String rsaPrivateKey ="-----BEGIN RSA PRIVATE KEY-----\n" +
+            "MIICXAIBAAKBgQC/nImKTiOJJ5NKGTsqYafE+V10fvZNHCtvP2VBkmiEx82d+IFJ\n" +
+            "22tPqg135vRVOOWeJs1igH+hH29vpUdnYUwQSfL9ougnpZHRN4KOpyAjMF95cpmb\n" +
+            "CMdYz/JYnjIBlmp8Vcx+nLtP15umJg7pKQQeh5Q7hyf1A+UajDoNucxtGwIDAQAB\n" +
+            "AoGADipXbLccH26ij+7PlfGEQqagxxT/xnGXsHDCXpehTHes9snBXSFwjIIo3r1f\n" +
+            "QNGlAA0kT+zqApJdXaadYcIqqkTdgLAahjbtEgJakax2YpaEpdcy9w+c7msGDZzf\n" +
+            "7blT16DNRgJ/lJhaPmGsFki4tbu7g6X/h2ocAhfDYf6ajBECQQDf3mtYzhXE87CV\n" +
+            "UdsbbUpDqf+8rM48QG8MpPahjJjgiMduhF0ojqY66seCelpqerLfKD6YbAunmsu5\n" +
+            "XYNuPhgjAkEA2xzj/245UtadXJJzPX7MnlZ0K7YLzdkuIzssQSX8KvDSFrxHTEAn\n" +
+            "olpjUdCwhJLoR4D/VgXc6OwxWieHtPBqqQJBANKS9yxAJS/t/MJnjY6d3YYKCNij\n" +
+            "PxZjkVE2dhp9EmvZnKWy8aGtlGTE5Ecx6YxQv6GyzDY5IzlTHJ/Oe0seMzsCQFAd\n" +
+            "2TP9fpJOjM1bcy2qHnFK1UFiWCCVZ6xUJ5NOq0ycL4VGpJ852gQgHq68qr5e5PW4\n" +
+            "MiSnEiZMrqLzT0iWm7kCQErFRnXpRiNkVTBE2EEa8KZO/anbJoe0GjScngJCgwuv\n" +
+            "lkqk555V9IF1war4VV9H9GPZ/Lt1WyWOMgf1kKPf7S8=\n" +
+            "-----END RSA PRIVATE KEY-----";
+
+    /**
+     * RSA 公钥
+     */
+    String rsaPublicKey = "-----BEGIN PUBLIC KEY-----\n" +
+            "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQC/nImKTiOJJ5NKGTsqYafE+V10\n" +
+            "fvZNHCtvP2VBkmiEx82d+IFJ22tPqg135vRVOOWeJs1igH+hH29vpUdnYUwQSfL9\n" +
+            "ougnpZHRN4KOpyAjMF95cpmbCMdYz/JYnjIBlmp8Vcx+nLtP15umJg7pKQQeh5Q7\n" +
+            "hyf1A+UajDoNucxtGwIDAQAB\n" +
+            "-----END PUBLIC KEY-----";
+}
---
+++ pingpp-demo/src/main/java/cn/stt/pingpp/controller/DemoTest.java
@@ -0,0 +1,73 @@
+package cn.stt.pingpp.controller;
+
+import cn.stt.pingpp.constant.Constants;
+import cn.stt.pingpp.example.Main;
+import com.pingplusplus.exception.APIConnectionException;
+import com.pingplusplus.exception.APIException;
+import com.pingplusplus.exception.AuthenticationException;
+import com.pingplusplus.exception.ChannelException;
+import com.pingplusplus.exception.InvalidRequestException;
+import com.pingplusplus.exception.RateLimitException;
+import com.pingplusplus.model.Charge;
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @Author shitongtong
+ * <p>
+ * Created by shitongtong on 2017/7/21.
+ */
+public class DemoTest {
+
+    private final String appId = Constants.appId;
+
+
+    /**
+     * 创建 charge
+     */
+    @Test
+    public void testCreateCharge(){
+        Charge charge = null;
+        String channel = "alipay";
+
+        Map<String, Object> chargeMap = new HashMap<String, Object>();
+        chargeMap.put("amount", 100);//订单总金额, 人民币单位：分（如订单总金额为 1 元，此处请填 100）
+        chargeMap.put("currency", "cny");
+        chargeMap.put("subject", "Your Subject");
+        chargeMap.put("body", "Your Body");
+        String orderNo = new Date().getTime() + Main.randomString(7);
+        chargeMap.put("order_no", orderNo);// 推荐使用 8-20 位，要求数字或字母，不允许其他字符
+        chargeMap.put("channel", channel);// 支付使用的第三方支付渠道取值，请参考：https://www.pingxx.com/api#api-c-new
+        chargeMap.put("client_ip", "127.0.0.1"); // 发起支付请求客户端的 IP 地址，格式为 IPV4，如: 127.0.0.1
+        Map<String, String> app = new HashMap<String, String>();
+        app.put("id", appId);
+        chargeMap.put("app", app);
+
+        // extra 取值请查看相应方法说明
+//        chargeMap.put("extra", channelExtra(channel));
+
+        try {
+            //发起交易请求
+            charge = Charge.create(chargeMap);
+            // 传到客户端请先转成字符串 .toString(), 调该方法，会自动转成正确的 JSON 字符串
+            String chargeString = charge.toString();
+            System.out.println(chargeString);
+        } catch (APIConnectionException e) {
+            e.printStackTrace();
+        } catch (ChannelException e) {
+            e.printStackTrace();
+        } catch (RateLimitException e) {
+            e.printStackTrace();
+        } catch (AuthenticationException e) {
+            e.printStackTrace();
+        } catch (APIException e) {
+            e.printStackTrace();
+        } catch (InvalidRequestException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
